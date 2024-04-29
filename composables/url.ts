77cade0c0af21f00270dae995fcc039300a40158
Rakha <115838117+RakhaaNZ@users.noreply.github.com>
2024-04-29 11:10:21 +0700

    final

--- composables/url.ts
+++ composables/url.ts
@@ -2,5 +2,5 @@ import { useRoute } from 'vitepress'
 
 export const getGithubUrl = (path?: string) => {
     const route = useRoute()
-    return `https://github.com/zuramai/refactoring/tree/main/docs/${route.path}/${path}`
+    return `${route.path}/${path}`
 }
